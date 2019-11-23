684d4d3db215eec83b69dd6993d253226227dc2b
Shrin <fuxay@qq.com>
2019-11-23 19:55:43 +0800

    完善内容

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from tencentcloud.common import credential
 from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException 
-from tencentcloud.ocr.v20181119 import ocr_client, models 
+from tencentcloud.ocr.v20181119 import ocr_client, models
+import json
+import handle
 try: 
     cred = credential.Credential("AKIDUq170gMH8l9IH3Cty3vZzB9uQgrnlw9f", "UjiT1OXZMvEH6g166MN8evzpPpBIAm9I") 
     httpProfile = HttpProfile()
@@ -16,8 +18,10 @@ try:
     params = '{"ImageUrl":"http://oken.club/capture.png"}'
     req.from_json_string(params)
 
-    resp = client.TableOCR(req) 
-    print(resp.to_json_string()) 
-
+    resp = client.TableOCR(req)
+    data = handle.handle(resp.to_json_string())
+    data = json.loads(data)
+    print(data)
+    
 except TencentCloudSDKException as err: 
-    print(err) 
+    print(err)
